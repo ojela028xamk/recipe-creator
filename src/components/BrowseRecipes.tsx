a6c52c90764767153d364d0bd8de7524c3c1cb34
Jere Länsipii <jerelans91@gmail.com>
2022-06-11 14:07:06 +0300

    modal added

--- src/components/BrowseRecipes.tsx
+++ src/components/BrowseRecipes.tsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import { MockData } from "../common/MockData";
 import { MockDataTS } from "../interfaces/MockDataTS";
 
 export default function BrowseRecipes(): JSX.Element {
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [modalData, setModalData] = useState<MockDataTS>();
+
+  function handleModal(data: MockDataTS) {
+    setShowModal(true);
+    setModalData(data);
+  }
+
   return (
     <div className="p-4">
       <h1>Browse Recipes</h1>
       {MockData.map((data: MockDataTS) => (
         <>
-          <div className="m-3 p-2 border w-50">
+          <div className="m-3 p-3 border w-50">
             <h3>{data.title}</h3>
-            <p>{data.instructions}</p>
+            <Button onClick={() => handleModal(data)}>Avaa</Button>
           </div>
         </>
       ))}
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>{modalData?.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{modalData?.instructions}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => setShowModal(false)}>
+            Sulje
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 }
