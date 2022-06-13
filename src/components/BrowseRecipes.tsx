b4e9decae934919303a19f9a6fdc5e38e53c9d3f
Jere Länsipii <jerelans91@gmail.com>
2022-06-13 16:11:05 +0300

    json-server added

--- src/components/BrowseRecipes.tsx
+++ src/components/BrowseRecipes.tsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
-import { MockData } from "../common/MockData";
-import { MockDataTS } from "../interfaces/MockDataTS";
+import { RecipeTS } from "../interfaces/RecipeTS";
 
 export default function BrowseRecipes(): JSX.Element {
+  const [recipes, setRecipes] = useState<RecipeTS[]>();
   const [showModal, setShowModal] = useState<boolean>(false);
-  const [modalData, setModalData] = useState<MockDataTS>();
+  const [modalData, setModalData] = useState<RecipeTS>();
 
-  function handleModal(data: MockDataTS) {
+  function handleModal(data: RecipeTS) {
     setShowModal(true);
     setModalData(data);
   }
 
+  // fetch recipes from json-server
+  useEffect(() => {
+    fetch("http://localhost:3004/recipes")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setRecipes(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="p-4">
       <h1>Browse Recipes</h1>
-      {MockData.map((data: MockDataTS) => (
-        <>
-          <div className="m-3 p-3 border w-50">
-            <h3>{data.title}</h3>
-            <Button onClick={() => handleModal(data)}>Avaa</Button>
-          </div>
-        </>
-      ))}
+      {recipes &&
+        recipes.map((recipe: RecipeTS) => (
+          <>
+            <div key={recipe.id} className="m-3 p-3 border w-50">
+              <h3>{recipe.title}</h3>
+              <Button onClick={() => handleModal(recipe)}>Avaa</Button>
+            </div>
+          </>
+        ))}
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
           <Modal.Title>{modalData?.title}</Modal.Title>
